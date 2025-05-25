302d5e67e217a85b49c1d8dc1acde0f027ebbb92
Ramon Salva Sanchez <ramonsalvasanchez@gmail.com>
2025-05-25 15:55:29 +0200

    chore: created components

---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+
+function User({ user, handle }) {
+  return (
+    <span className="user">
+      <span className="name">{user}</span>
+      <span className="handle">{handle}</span>
+    </span>
+  );
+}
+
+export default User;
